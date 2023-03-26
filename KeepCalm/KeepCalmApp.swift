cda22b4698e40f5025afbaeb4954a2bbab49594d
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-03-26 19:20:27 +0200

    Initial Commit

---
+++ KeepCalm/KeepCalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepCalmApp.swift
+//  KeepCalm
+//
+//  Created by Rafal Pawelec on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct KeepCalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
